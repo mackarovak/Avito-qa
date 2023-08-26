cc1ca5dc4b1090218beae3ab5598783f5f44f477
mackarovak <ksenija.mackarova2014@yandex.ru>
2023-08-26 19:00:52 +0300

    [nuancce] появилась функция удаления

--- tasks/test.py
+++ tasks/test.py
@@ -37,18 +37,20 @@ favorites_list = WebDriverWait(driver, 10).until(
     EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div[1]/div/div/div[1]/a/img'))
 )
 
-driver.get("https://www.avito.ru/all/kollektsionirovanie?q=%D0%9C%D0%BE%D0%BD%D0%B5%D1%82%D0%B0+50%D0%BB%D0%B5%D1%82")
-add_to_favorites_button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="i3363777565"]/div/div/div[2]/div[1]/div'))
-)
-add_to_favorites_button.click()
+# driver.get("https://www.avito.ru/all/kollektsionirovanie?q=%D0%9C%D0%BE%D0%BD%D0%B5%D1%82%D0%B0+50%D0%BB%D0%B5%D1%82")
+# add_to_favorites_button = WebDriverWait(driver, 10).until(
+  #  EC.presence_of_element_located((By.XPATH, '//*[@id="i3363777565"]/div/div/div[2]/div[1]/div'))
+# )
+# add_to_favorites_button.click()
+
 
 driver.get("https://www.avito.ru/favorites")
-remove_to_favorites_button = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div[1]/div/div/div[2]/div[1]/div'))
-)
+item_to_remove = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div/div/div/div[2]')
+remove_button = item_to_remove.find_element(By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div/div/div/div[2]/div[1]/div')
+remove_button.click()
 
-remove_to_favorites_button.click()
+driver.get("https://www.avito.ru/kaliningrad/kollektsionirovanie/moneta_50let_3363777565")
 
+driver.get("https://www.avito.ru/favorites")
 
 driver.quit()
