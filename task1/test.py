2a3a88f9aeebbaa8c361d54ee6872e7324ccb32e
mackarovak <ksenija.mackarova2014@yandex.ru>
2023-08-26 17:12:39 +0300

    [nuancce] 1 правка

---
+++ task1/test.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+driver = webdriver.Chrome()
+
+driver.get("https://www.avito.ru/favorites")
+
+title = driver.title
+
+expected_title = "Объявления | Избранное | Авито"
+if title == expected_title:
+    print("Заголовок страницы совпадает с ожидаемым значением")
+else:
+    print("Заголовок страницы не совпадает с ожидаемым значением")
+
+driver.get("https://www.avito.ru/kaliningrad/kollektsionirovanie/moneta_50let_3363777565")
+add_to_favorites_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[3]/div[1]/div/div[2]/div[3]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/button/span'))
+)
+
+add_to_favorites_button.click()
+driver.get("https://www.avito.ru/favorites")
+favorites_list = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div[1]/div/div/div[1]/a/img'))
+)
+
+driver.get("https://www.avito.ru/slavyansk-na-kubani/telefony/telefon_samsung_s21fe5g_3254210395")
+add_to_favorites_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[3]/div[1]/div/div[2]/div[3]/div[1]/div[1]/div/div[3]/div/div/div/div[1]/button/span'))
+)
+
+add_to_favorites_button.click()
+driver.get("https://www.avito.ru/favorites")
+favorites_list = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div[1]/div/div/div[1]/a/img'))
+)
+
+driver.get("https://www.avito.ru/all/kollektsionirovanie?q=%D0%9C%D0%BE%D0%BD%D0%B5%D1%82%D0%B0+50%D0%BB%D0%B5%D1%82")
+add_to_favorites_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="i3363777565"]/div/div/div[2]/div[1]/div'))
+)
+add_to_favorites_button.click()
+
+driver.get("https://www.avito.ru/favorites")
+remove_to_favorites_button = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div[1]/div/div/div[2]/div[1]/div'))
+)
+
+remove_to_favorites_button.click()
+
+
+driver.quit()
