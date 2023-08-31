1bd2ca953269e6b554875188a928a3ac1a732b67
mackarovak <ksenija.mackarova2014@yandex.ru>
2023-08-31 10:04:45 +0300

    работает удаление

--- tasks/test.py
+++ tasks/test.py
@@ -8,9 +8,9 @@ import time
 
 # Initialize the driver
 driver = webdriver.Chrome()
-
-def test_avito_favorites_page_title():
-    driver.get("https://www.avito.ru/favorites")
+url = "https://www.avito.ru/favorites"
+def test_favorites_page_title():
+    driver.get(url)
     WebDriverWait(driver, 10).until(
         EC.title_is("Объявления | Избранное | Авито")
     )
@@ -21,8 +21,8 @@ def test_avito_favorites_page_title():
     except AssertionError:
         print("Заголовок страницы не совпадает с ожидаемым значением")
 
-def test_add_item_to_favorites(url):
-    driver.get(url)
+def test_add_item_to_favorites(item_url):
+    driver.get(item_url)
 
     # Find the "Add to favorites" button and click it
     add_to_favorites_button = WebDriverWait(driver, 10).until(
@@ -31,7 +31,7 @@ def test_add_item_to_favorites(url):
     add_to_favorites_button.click()
 
     # Go to the favorites page
-    driver.get("https://www.avito.ru/favorites")
+    driver.get(url)
 
     # Check if the item is added to the favorites list
     favorites_list = WebDriverWait(driver, 10).until(
@@ -43,27 +43,26 @@ def test_add_item_to_favorites(url):
     except AssertionError:
         print("Товар не добавлен в избранное")
 
-def remove_item(url, xpath):
+def test_remove_item(url, xpath):
     driver.get(url)
     item_to_remove = driver.find_element(By.XPATH, xpath)
     remove_button = item_to_remove.find_element(By.XPATH, './div[1]/div')
     remove_button.click()
-
-
-    if driver.find_element(By.XPATH, xpath):
-        print("Товар удален из избранного")
-    else:
-        print("Товар не удален из избранного")
+    try:
+        driver.find_element(By.XPATH, xpath)
+        assert True
+    except NoSuchElementException:
+        assert False, "Товар не удален из избранного"
 
 
 def test_sorting():
-    driver.get("https://www.avito.ru/favorites")
+    driver.get(url)
     price_checkbox = driver.find_element(By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[1]/div/div/div[2]/div[1]')
     price_checkbox.click()
     print("Товары из избанного отсортированы по давности добавления")
 
 # Add item to favorites
-def add_to_favorites(item_url, xpath):
+def test_add_to_favorites(item_url, xpath):
     driver.get(item_url)
 
     add_to_favorites_button = WebDriverWait(driver, 10).until(
@@ -71,7 +70,7 @@ def add_to_favorites(item_url, xpath):
     )
     add_to_favorites_button.click()
 
-    driver.get("https://www.avito.ru/favorites")
+    driver.get(url)
 
     favorites_list = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div[1]/div/div/div[1]/a/img'))
@@ -83,8 +82,7 @@ def add_to_favorites(item_url, xpath):
         print("Товар не добавлен в избранное")
 
 try:
-    test_avito_favorites_page_title()
-    url = "https://www.avito.ru/favorites"
+    test_favorites_page_title()
     xpath = '//*[@id="app"]/div/div[4]/div/div/favorite-items-list/div/div/div[1]/div[2]/div/div/div/div[2]'
 # Run first test
     url1 = "https://www.avito.ru/kaliningrad/kollektsionirovanie/moneta_50let_3363777565"
@@ -101,14 +99,14 @@ try:
     test_sorting()
 
 # Remove items from favorites
-    remove_item(url, xpath)
-    remove_item(url, xpath)
-    remove_item(url, xpath)
+    test_remove_item(url, xpath)
+    test_remove_item(url, xpath)
+    test_remove_item(url, xpath)
 
 # Add new items to favorites
-    item_url = "https://www.avito.ru/all/kollektsionirovanie?q=%D0%9C%D0%BE%D0%BD%D0%B5%D1%82%D0%B0+50%D0%BB%D0%B5%D1%82"
-    add_to_favorites(item_url, '//*[@id="i3363777565"]/div/div/div[2]/div[1]/div')
-    add_to_favorites('https://www.avito.ru/all/odezhda_obuv_aksessuary?q=%D0%9A%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8+Dolce%26Gabbana+%D0%B6%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%D0%B5', '//*[@id="i3182581196"]/div/div/div[2]/div[1]/div')
+    poisk_url = "https://www.avito.ru/all/kollektsionirovanie?q=%D0%9C%D0%BE%D0%BD%D0%B5%D1%82%D0%B0+50%D0%BB%D0%B5%D1%82"
+    test_add_to_favorites(poisk_url, '//*[@id="i3363777565"]/div/div/div[2]/div[1]/div')
+    test_add_to_favorites('https://www.avito.ru/all/odezhda_obuv_aksessuary?q=%D0%9A%D1%80%D0%BE%D1%81%D1%81%D0%BE%D0%B2%D0%BA%D0%B8+Dolce%26Gabbana+%D0%B6%D0%B5%D0%BD%D1%81%D0%BA%D0%B8%D0%B5', '//*[@id="i3182581196"]/div/div/div[2]/div[1]/div')
     time.sleep(10)
 finally:
     driver.quit()
